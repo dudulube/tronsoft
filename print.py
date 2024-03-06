8d231958e899b27e74fdc28deca89f3fafeac4e1
dudulube <dudulube@outlook.com>
2024-03-06 10:59:36 -0300

    Update print.py

--- print.py
+++ print.py
@@ -1,14 +1,21 @@
 import streamlit as st
-import subprocess
 
-def abrir_calculadora():
-    st.title("Abrir Calculadora do Windows")
+def menu():
+    st.title("Menu com 3 Botões")
 
-    if st.button("Abrir Calculadora"):
-        try:
-            subprocess.run(["C:\Windows\System32\calc.exe"])  # Comando para abrir la calculadora de Windows
-        except Exception as e:
-            st.error(f"Não foi possível abrir a calculadora: {e}")
+    # Botões no menu
+    opcao = st.sidebar.radio("Selecione uma opção:", ["Botão 1", "Botão 2", "Botão 3"])
+
+    # Conteúdo conforme a opção selecionada
+    if opcao == "Botão 1":
+        st.header("Conteúdo do Botão 1")
+        st.write("Este é o conteúdo do Botão 1.")
+    elif opcao == "Botão 2":
+        st.header("Conteúdo do Botão 2")
+        st.write("Este é o conteúdo do Botão 2.")
+    elif opcao == "Botão 3":
+        st.header("Conteúdo do Botão 3")
+        st.write("Este é o conteúdo do Botão 3.")
 
 if __name__ == "__main__":
-    abrir_calculadora()
+    menu()
