95d1ceb5bf84bc160edce80c75f96c62d2495b6a
dudulube <dudulube@outlook.com>
2024-03-06 10:56:11 -0300

    Update print.py

--- print.py
+++ print.py
@@ -2,13 +2,13 @@ import streamlit as st
 import subprocess
 
 def abrir_calculadora():
-    st.title("Abrir Calculadora de Windows")
+    st.title("Abrir Calculadora do Windows")
 
     if st.button("Abrir Calculadora"):
         try:
             subprocess.run(["calc.exe"])  # Comando para abrir la calculadora de Windows
         except Exception as e:
-            st.error(f"Error al abrir la calculadora: {e}")
+            st.error(f"Não foi possível abrir a calculadora: {e}")
 
 if __name__ == "__main__":
     abrir_calculadora()
