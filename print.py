fb78f0a4151acd6791eed472ff8c6b817aae836b
dudulube <dudulube@outlook.com>
2024-03-06 14:52:32 -0300

    Update print.py

--- print.py
+++ print.py
@@ -1,21 +1,49 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-def menu():
-    st.title("Menu com 3 Botões")
+# 1. as sidebar menu
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
 
-    # Botões no menu
-    opcao = st.sidebar.radio("Selecione uma opção:", ["Botão 1", "Botão 2", "Botão 3"])
+# 2. horizontal menu
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+selected2
 
-    # Conteúdo conforme a opção selecionada
-    if opcao == "Botão 1":
-        st.header("Conteúdo do Botão 1")
-        st.write("Este é o conteúdo do Botão 1.")
-    elif opcao == "Botão 2":
-        st.header("Conteúdo do Botão 2")
-        st.write("Este é o conteúdo do Botão 2.")
-    elif opcao == "Botão 3":
-        st.header("Conteúdo do Botão 3")
-        st.write("Este é o conteúdo do Botão 3.")
+# 3. CSS style definitions
+selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    }
+)
 
-if __name__ == "__main__":
-    menu()
+# 4. Manual item selection
+if st.session_state.get('switch_button', False):
+    st.session_state['menu_option'] = (st.session_state.get('menu_option', 0) + 1) % 4
+    manual_select = st.session_state['menu_option']
+else:
+    manual_select = None
+    
+selected4 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    orientation="horizontal", manual_select=manual_select, key='menu_4')
+st.button(f"Move to Next {st.session_state.get('menu_option', 1)}", key='switch_button')
+selected4
+
+# 5. Add on_change callback
+def on_change(key):
+    selection = st.session_state[key]
+    st.write(f"Selection changed to {selection}")
+    
+selected5 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],
+                        icons=['house', 'cloud-upload', "list-task", 'gear'],
+                        on_change=on_change, key='menu_5', orientation="horizontal")
+selected5
