abc47829a561c3537d475d1682243373ee9cfa8f
dudulube <dudulube@outlook.com>
2024-03-06 14:55:16 -0300

    Update print.py

--- print.py
+++ print.py
@@ -1,49 +1,61 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-option-menu",
+    version="0.3.12",
+    author="Victor Yan",
+    author_email="victoryhb@163.com",
+    description="streamlit-option-menu is a simple Streamlit component that allows users to select a single item from a list of options in a menu.",
+    long_description="""streamlit-option-menu is a simple Streamlit component that allows users to select a single item from a list of options in a menu.
+It is similar in function to st.selectbox(), except that:
+- It uses a simple static list to display the options instead of a dropdown
+- It has configurable icons for each option item and the menu title
+
+It is built on [streamlit-component-template-vue](https://github.com/andfanilo/streamlit-component-template-vue), styled with [Bootstrap](https://getbootstrap.com/) and with icons from [bootstrap-icons](https://icons.getbootstrap.com/)
+
+## Installation
+```
+pip install streamlit-option-menu
+```
+
+## Parameters
+The `option_menu` function accepts the following parameters:
+- menu_title (required): the title of the menu; pass None to hide the title
+- options (required): list of (string) options to display in the menu; set an option to "---" if you want to insert a section separator
+- default_index (optional, default=0): the index of the selected option by default
+- menu_icon (optional, default="menu-up"): name of the [bootstrap-icon](https://icons.getbootstrap.com/) to be used for the menu title
+- icons (optional, default=["caret-right"]): list of [bootstrap-icon](https://icons.getbootstrap.com/) names to be used for each option; its length should be equal to the length of options
+- orientation (optional, default="vertical"): "vertical" or "horizontal"; whether to display the menu vertically or horizontally
+
+The function returns the (string) option currently selected
+
+## Example
+```
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-# 1. as sidebar menu
 with st.sidebar:
     selected = option_menu("Main Menu", ["Home", 'Settings'], 
         icons=['house', 'gear'], menu_icon="cast", default_index=1)
     selected
 
-# 2. horizontal menu
+# horizontal Menu
 selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
     icons=['house', 'cloud-upload', "list-task", 'gear'], 
     menu_icon="cast", default_index=0, orientation="horizontal")
-selected2
-
-# 3. CSS style definitions
-selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal",
-    styles={
-        "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "green"},
-    }
+    selected2
+```
+""",
+    long_description_content_type="text/plain",
+    url="https://github.com/victoryhb/streamlit-option-menu",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
 )
-
-# 4. Manual item selection
-if st.session_state.get('switch_button', False):
-    st.session_state['menu_option'] = (st.session_state.get('menu_option', 0) + 1) % 4
-    manual_select = st.session_state['menu_option']
-else:
-    manual_select = None
-    
-selected4 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    orientation="horizontal", manual_select=manual_select, key='menu_4')
-st.button(f"Move to Next {st.session_state.get('menu_option', 1)}", key='switch_button')
-selected4
-
-# 5. Add on_change callback
-def on_change(key):
-    selection = st.session_state[key]
-    st.write(f"Selection changed to {selection}")
-    
-selected5 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],
-                        icons=['house', 'cloud-upload', "list-task", 'gear'],
-                        on_change=on_change, key='menu_5', orientation="horizontal")
-selected5
