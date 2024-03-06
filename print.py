1376913b2d948199be56c2c1b63a22bf6bcd3685
dudulube <dudulube@outlook.com>
2024-03-06 10:54:30 -0300

    Update print.py

--- print.py
+++ print.py
@@ -1,29 +1,14 @@
 import streamlit as st
+import subprocess
 
-def calculadora():
-    st.title("Calculadora Simples")
+def abrir_calculadora():
+    st.title("Abrir Calculadora de Windows")
 
-    num1 = st.number_input("Digite o primeiro número", step=1)
-    operacion = st.selectbox("Qual a operação?", ["Soma", "Subtração", "Multiplicação", "Divisão"])
-    num2 = st.number_input("Digite o segundo número", step=1)
-
-    if st.button("Calcular"):
-        resultado = realizar_calculo(num1, num2, operacion)
-        st.success(f"O resultado de {num1} {operacion} {num2} é: {resultado}")
-
-def realizar_calculo(num1, num2, operacion):
-    if operacion == "Soma":
-        return num1 + num2
-    elif operacion == "Subtração":
-        return num1 - num2
-    elif operacion == "Multiplicação":
-        return num1 * num2
-    elif operacion == "Divisão":
-        if num2 != 0:
-            return num1 / num2
-        else:
-            st.error("¡Erro! Não se pode dividir por zero.")
-            return None
+    if st.button("Abrir Calculadora"):
+        try:
+            subprocess.run(["calc.exe"])  # Comando para abrir la calculadora de Windows
+        except Exception as e:
+            st.error(f"Error al abrir la calculadora: {e}")
 
 if __name__ == "__main__":
-    calculadora()
+    abrir_calculadora()
